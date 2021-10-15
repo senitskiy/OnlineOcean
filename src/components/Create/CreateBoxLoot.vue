4c54a3af502c448aa4e22b8e9f34dbeea5843a44
websorokinweb <bohdansorokin7@gmail.com>
2021-10-16 00:48:31 +0300

    Added create-box-loot

---
+++ src/components/Create/CreateBoxLoot.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="create-modal__box-loot box-loot">
+    <div class="box-loot__items">
+      <app-art
+      short
+      view='box-loot'
+      ></app-art>
+      <app-art
+      short
+      view='box-loot'
+      ></app-art>
+      <app-art
+      short
+      view='box-loot'
+      ></app-art>
+      <button class="box-loot__add btn-clear">
+        <span class="box-loot__imgwrapper">
+          <img src="@/assets/images/upload.svg" alt="">
+        </span>
+        <span class="box-loot__add-descr">
+          {{ data.uploadDescr }}
+        </span>
+      </button>
+    </div>
+    <p class="box-loot__descr">
+      {{ data.title }}
+    </p>
+  </div>
+</template>
+
+<script>
+import AppArt from '@/components/App/AppArt.vue';
+
+export default {
+  data() {
+    return {
+      data:{
+        title: 'Items are loaded from the offers you created earlier.',
+        uploadDescr: 'Upload item',
+      }
+    }
+  },
+  components: {
+    AppArt,
+  },
+}
+</script>
--- src/views/CreateSomething.vue
+++ src/views/CreateSomething.vue
@@ -2,6 +2,16 @@
   <section class="create-modal">
     <div class="container">
       <div class="create-modal__inner">
+        <app-button
+        title='Back'
+        hasSlot
+        view='back'
+        @click='routePrev()'
+        >
+          <svg width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.223538 6.45372L6.45371 0.223657C6.59781 0.0794465 6.79017 0 6.99527 0C7.20037 0 7.39273 0.0794465 7.53683 0.223657L7.99564 0.682352C8.29419 0.981244 8.29419 1.46703 7.99564 1.76547L2.764 6.9971L8.00144 12.2345C8.14554 12.3787 8.2251 12.571 8.2251 12.776C8.2251 12.9812 8.14554 13.1734 8.00144 13.3178L7.54263 13.7763C7.39842 13.9206 7.20618 14 7.00108 14C6.79597 14 6.60362 13.9206 6.45952 13.7763L0.223538 7.54059C0.0791006 7.39592 -0.000231743 7.20277 0.00022316 6.99744C-0.000231743 6.79131 0.0791006 6.59827 0.223538 6.45372Z" fill="#D4D4D4"/>
+          </svg>
+        </app-button>
         <div class="create-modal__imgwrapper">
           <img src="@/assets/images/create-box.svg" alt="">
         </div>
@@ -63,7 +73,7 @@
             ></app-big-art>
           </div>
         </div>
-        <create-box-items></create-box-items>
+        <create-box-loot></create-box-loot>
         <create-chars></create-chars>
       </div>
     </div>
@@ -75,7 +85,7 @@ import AppInput from '@/components/App/AppInput.vue';
 import AppBigArt from '@/components/App/AppBigArt.vue';
 // import AppSelect from '@/components/App/AppSelect.vue';
 import CreateChoose from '@/components/Create/CreateChoose.vue';
-import CreateBoxItems from '@/components/Create/CreateBoxItems.vue';
+import CreateBoxLoot from '@/components/Create/CreateBoxLoot.vue';
 import CreateChars from '@/components/Create/CreateChars.vue';
 
 export default {
@@ -107,6 +117,9 @@ export default {
     }
   },
   methods: {
+    routePrev(){
+      this.$router.go(-1)
+    },
     setChoosed(value) {
       console.log(value)
       // this.data.chooses = value
@@ -127,7 +140,7 @@ export default {
     AppInput,
     AppBigArt,
     CreateChoose,
-    CreateBoxItems,
+    CreateBoxLoot,
     CreateChars,
     // AppSelect,
   },
