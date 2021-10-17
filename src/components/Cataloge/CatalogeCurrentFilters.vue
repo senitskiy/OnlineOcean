f5cc4e5637aeb7f290c96a19b436b1e6e550fe3c
websorokinweb <bohdansorokin7@gmail.com>
2021-10-17 23:56:44 +0300

    Added Current Filters

--- src/components/Cataloge/CatalogeCurrentFilters.vue
+++ src/components/Cataloge/CatalogeCurrentFilters.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-
+  <div class="cataloge__current">
+    <p class="cataloge__current-all">
+      {{ allItems }}
+    </p>
+    <div class="cataloge__current-filters">
+      <button class="cataloge__current-filter btn-clear">
+        ETC
+        <img src="@/assets/images/close.svg" alt="">
+      </button>
+      <button class="cataloge__current-filter btn-clear">
+        Action
+        <img src="@/assets/images/close.svg" alt="">
+      </button>
+      <button class="cataloge__current-reset btn-clear">
+        Clear All
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      allItems: '1 412 515 results',
+    }
+  },
 }
 </script>
