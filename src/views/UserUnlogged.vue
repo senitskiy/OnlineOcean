53e2df6907e266d93f2436c3bb01b086e639396e
websorokinweb <bohdansorokin7@gmail.com>
2021-10-29 00:19:40 +0300

    Done UserConnect

--- src/views/UserUnlogged.vue
+++ src/views/UserUnlogged.vue
@@ -1,13 +1,16 @@
 <template>
   <app-cover></app-cover>
+  <user-connect></user-connect>
 </template>
 
 <script>
 import AppCover from '@/components/App/AppCover.vue'
+import UserConnect from '@/components/User/UserConnect.vue'
 
 export default {
   components: {
     AppCover,
+    UserConnect,
   },
 }
 </script>
