78bfcddf0c8839ce79b5279f48468ed3e7234aeb
websorokinweb <bohdansorokin7@gmail.com>
2021-10-29 17:19:09 +0300

    User layout don

--- src/components/User/UserCover.vue
+++ src/components/User/UserCover.vue
@@ -10,9 +10,10 @@
       <div class="user-cover__inner">
         <div class="user-cover__profile">
           <app-profile
-          userId='34'
+          :userId='content.userId'
+          view='cover'
           ></app-profile>
-          <div class="user-cover__profile-col">
+          <div class="user-cover__profile-col user-cover__profile-loot">
             <p class="user-cover__profile-username">
               {{ content.userName }}
             </p>
@@ -31,12 +32,13 @@
               </li>
             </ul>
           </div>
-          <div class="user-cover__profile-col">
+          <div class="user-cover__profile-col user-cover__profile-edit">
             <div class="user-cover__profile-copy copy"
-            @click='copyId(this.content.userId)'
+            @click='copyToken(this.content.userToken)'
             >
               <input class="copy__input" type="text"
-              :value='shortId'
+              :value='shortToken'
+              disabled
               >
               <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M14.5 0H2.5C1.4 0 0.5 0.9 0.5 2V16H2.5V2H14.5V0ZM17.5 4H6.5C5.4 4 4.5 4.9 4.5 6V20C4.5 21.1 5.4 22 6.5 22H17.5C18.6 22 19.5 21.1 19.5 20V6C19.5 4.9 18.6 4 17.5 4ZM17.5 20H6.5V6H17.5V20Z" fill="#EA4C89"/>
@@ -44,6 +46,7 @@
             </div>
             <app-button
             :title='content.btnTitle'
+            view='lined'
             ></app-button>
           </div>
         </div>
@@ -61,7 +64,8 @@ export default {
       content:{
         btnTitle: 'Edit profile',
         userName: 'ArtStudio_nft',
-        userId: '4b73hghjk4ljh2jk3hy956',
+        userId: 34,
+        userToken: '4b73hghjk4ljh2jk3hy956',
         userLootItemsLabel: 'Items',
         userLootBoxesLabel: 'Boxes',
         userLootItems: 134,
@@ -70,14 +74,14 @@ export default {
     }
   },
   methods: {
-    copyId(value) {
+    copyToken(value) {
       navigator.clipboard.writeText(value);
     }
   },
   computed: {
-    shortId() {
-      let first = this.content.userId.slice(0, 4),
-          second = this.content.userId.slice(-4, this.content.userId.length);
+    shortToken() {
+      let first = this.content.userToken.slice(0, 4),
+          second = this.content.userToken.slice(-4, this.content.userToken.length);
 
       return first + '...' + second
     }
