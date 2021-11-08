52cf6daeababb483bbbd2a11ceb595d25e90786a
websorokinweb <bohdansorokin7@gmail.com>
2021-11-08 18:32:08 +0200

    Cataloge Autoload

--- src/components/Cataloge/CatalogeRows.vue
+++ src/components/Cataloge/CatalogeRows.vue
@@ -1,29 +1,63 @@
 <template>
-  <div class="cataloge__rows"
+  <infinite-scroll
+  @infinite-scroll="loadDataFromServer"
+  :message="message"
+  :noResult="noResult"
   >
-    <app-art
-    v-for='id in info.items'
-    :artId='id'
-    :key='id'
-    ></app-art>
-  </div>
+    <div class="cataloge__rows">
+      <app-art
+      v-for='id in items'
+      :artId='id'
+      :key='id'
+      ></app-art>
+    </div>
+  </infinite-scroll>
 </template>
 
 <script>
 import AppArt from '@/components/App/AppArt.vue';
 
+import InfiniteScroll from "infinite-loading-vue3";
+
+// import axios from 'axios';
+
 export default {
   data() {
     return {
-      info:{
-        items:[
-          3, 55, 1, 3, 56, 94, 4, 3, 55, 1, 3, 56, 94, 43, 55, 1, 3, 56, 94, 4, 94, 43, 55, 1, 3
-        ]
+      items:[
+        3, 55, 1, 3, 56, 94, 4, 3, 55, 1, 3, 56, 94, 43, 55, 1, 3, 56, 94, 4, 94, 43, 55, 1, 3
+      ],
+      page: 1,
+      message: '',
+      noResult: false,
+    }
+  },
+  mounted () {
+    this.loadPosts()
+  },
+  methods: {
+    loadPosts(){
+      
+    },
+    async loadDataFromServer(){
+      try {
+        // const result = await axios.get(`/getItems=${this.page}`)
+        const result = [4554, 4, 2, 5, 3, 5, 5, 3, 6]
+        if(result.length) {
+          this.items.push(...result);
+        } else {
+          this.noResult = true;
+          this.message = "No result found";
+        }
+      } catch(err) {
+        this.noResult = true;
+        this.message = "Error loading data";
       }
     }
   },
   components: {
     AppArt,
+    InfiniteScroll,
   },
 }
 </script>
