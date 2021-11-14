627d37f85591b0db775a072a0b283d2dac2186da
websorokinweb <bohdansorokin7@gmail.com>
2021-11-14 21:07:41 +0200

    Changed deploy.sh & vue.config.js

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,15 @@
-# #!/usr/bin/env sh
+#!/usr/bin/env sh
 
-# set -e
+set -e
 
-# npm run build
+npm run build
 
-# cd dist
+cd dist
 
-# git init
-# git add -A
-# git commit -m 'New deployment'
+git init
+git add -A
+git commit -m 'New deployment'
 
-<<<<<<< HEAD
-# git push -f git@github.com:OnlineOcen/OnlineOcean.git main:gh-pages
-=======
-git push -f git@github.com:OnlineOcen/OnlineOcean.git front:gh-pages
->>>>>>> ea7db7aa2804c43b155f96bc5af51f277a1ec8e2
+git push -f git@github.com:OnlineOcen/OnlineOcean.git main:gh-pages
 
-# cd -
+cd -
